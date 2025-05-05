6a20505bc7fe301f454ba3b872724110026f5e94
viksit-siddhant <s.r.viksit@gmail.com>
2025-05-06 00:15:34 +0530

    cringe

--- imdb.py
+++ imdb.py
@@ -1,9 +1,14 @@
 import torch
 import torch.nn as nn
-from torchtext.datasets import IMDB
-from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator
+import datasets
 from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased") # Example tokenizer
+
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
 
 
 def yield_tokens(data_iter):
@@ -38,19 +43,40 @@ def collate_batch(batch):
 
 
 
-train_dataset = IMDB(root='.', split='train')
-tokenizer = get_tokenizer('basic_english')
-vocab = build_vocab_from_iterator(
-    yield_tokens(train_dataset),
-    min_freq=20,
-    specials=["<unk>", "<pad>"]
-)
-vocab.set_default_index(vocab["<unk>"])
-PAD_IDX = vocab['<pad>']
+imdb = datasets.load_dataset('imdb', split='train')
+tokenized_imdb = imdb.map(preprocess_function, batched=True)
 
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        #return len(self.data)
+        return 500
+
+    def __getitem__(self, idx):
+        idx = idx*12395234 + 183349213
+        idx = idx % len(self.data)
+        item = self.data[idx]
+        text = item['input_ids']
+        label = item['label']
+        return torch.tensor(text), torch.tensor(label)
+    
+dataset = Dataset(tokenized_imdb)
+train_size = int(0.99 * len(dataset))
+val_size = len(dataset) - train_size
+train,val = torch.utils.data.random_split(dataset, [train_size, val_size])
 train_dataloader = DataLoader(
-    train_dataset,
+    train,
     batch_size=1,
     shuffle=True,
-    collate_fn=collate_batch
 )
+val_dataloader = DataLoader(
+    val,
+    batch_size=32,
+    shuffle=True,
+)
+
+# Define the vocabulary size and padding index
+vocab_size = len(tokenizer)
+PAD_IDX = tokenizer.pad_token_id
--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-import torchtext
 from torch import nn
 from optim import *
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import random
 import numpy as np
-from imdb import train_dataloader, vocab
+from imdb import train_dataloader, vocab_size, PAD_IDX 
 
 def moving_average(data, k):
     data = np.array(data)
     return np.convolve(data, np.ones(k)/k, mode='valid')
 
 # Define a simple CNN for MNIST
-model = nn.Sequential(
-    nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
-    nn.ReLU(),
-    nn.MaxPool2d(kernel_size=2, stride=2),
-    nn.Flatten(),
-    nn.Linear(14*14*32, 10)
-)
 
 loss_fn = nn.CrossEntropyLoss()
 
-def loop(dataloader, model, loss_fn, optimizer, lr = 0.001,epochs=1, averaging = False):
+def loop(dataloader,valid_loader, model, loss_fn, optimizer, lr = 0.001,epochs=1, averaging = False):
     optim = optimizer(model.parameters(), lr=lr)
     if averaging:
         avg = Averager(model)
     loss_curve = []
+    valid_curve = []
     for epoch in range(epochs):
         print(f"Epoch {epoch+1}/{epochs}")
         for X,y in tqdm(dataloader):
@@ -43,12 +37,24 @@ def loop(dataloader, model, loss_fn, optimizer, lr = 0.001,epochs=1, averaging =
                 avg.update(model)
                 model = avg.get_model()
             loss_curve.append(loss.item())
-    return loss_curve
+            with torch.no_grad():
+                if random.random()<0.0001:
+                    acc = 0
+                    for X_val,y_val in valid_loader:
+                        #validation accuracy
+                        pred_val = model(X_val)
+                        pred_val = pred_val.argmax(dim=1)
+                        
+                        acc += (pred_val == y_val).float().mean()
+                    acc /= len(valid_loader)
+                    valid_curve.append(acc.item())
+    return loss_curve, valid_curve
 
-def two_loop_optimizer(dataloader, model, loss_fn, optimizer1, optimizer2, lr = 0.001, epochs=1, threshold=0.1):
+def two_loop_optimizer(dataloader, valid_loader, model, loss_fn, optimizer1, optimizer2, lr = 0.001, epochs=1, threshold=0.1):
     optim1 = optimizer1(model.parameters(), lr=lr)
     optim2 = optimizer2(model.parameters(), lr=lr)
     loss_curve = []
+    valid_curve = []
     for epoch in range(epochs):
         print(f"Epoch {epoch+1}/{epochs}")
         for i,(X,y) in tqdm(enumerate(dataloader)):
@@ -65,12 +71,26 @@ def two_loop_optimizer(dataloader, model, loss_fn, optimizer1, optimizer2, lr = 
                 loss.backward()
                 optim2.step()
             loss_curve.append(loss.item())
-    return loss_curve
+            with torch.no_grad():
+                if random.random()<0.0001:
+                    acc = 0
+                    for X_val,y_val in valid_loader:
+                        #validation accuracy
+                        pred_val = model(X_val)
+                        pred_val = pred_val.argmax(dim=1)
+                        
+                        acc += (pred_val == y_val).float().mean()
+                    acc /= len(valid_loader)
+                    valid_curve.append(acc.item())
+    return loss_curve, valid_curve
 
 
-def zeroth_order_loop(dataloader, model, loss_fn, optimizer, lr = 0.001, epochs=1):
+def zeroth_order_loop(dataloader, valid_loader, model, loss_fn, optimizer, lr = 0.001, epochs=1, averaging = False):
     optim = optimizer(model.parameters(), lr=lr, loss_fn=loss_fn, rand_directions=2)
     loss_curve = []
+    valid_curve = []
+    if averaging:
+        avg = Averager(model)
     for epoch in range(epochs):
         print(f"Epoch {epoch+1}/{epochs}")
         for X,y in tqdm(dataloader):
@@ -80,7 +100,21 @@ def zeroth_order_loop(dataloader, model, loss_fn, optimizer, lr = 0.001, epochs=
             optim.step(model, X, y)
             #print(f"Loss: {loss.item()}")
             loss_curve.append(loss.item())
-    return loss_curve
+            if averaging:
+                avg.update(model)
+                model = avg.get_model()
+            with torch.no_grad():
+                if random.random()<0.0001:
+                    acc = 0
+                    for X_val,y_val in valid_loader:
+                        #validation accuracy
+                        pred_val = model(X_val)
+                        pred_val = pred_val.argmax(dim=1)
+                        
+                        acc += (pred_val == y_val).float().mean()
+                    acc /= len(valid_loader)
+                    valid_curve.append(acc.item())
+    return loss_curve, valid_curve
 
 # Load MNIST dataset
 transform = transforms.Compose([
@@ -88,28 +122,207 @@ transform = transforms.Compose([
     transforms.Normalize((0.1307,), (0.3081,))
 ])
 train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+#split the dataset into train and validation sets
+train_size = int(0.99 * len(train_dataset))
+val_size = len(train_dataset) - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_size, val_size])
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-optimizers = [CustomOptimizer]
-plt.figure(figsize=(10, 6))
+valid_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+optimizers = [CustomOptimizer, AdagradOptimizer, AdamOptimizer, PAdagradOptimizer, [AdamOptimizer, CustomOptimizer]]
+for i,optimizer in enumerate(optimizers):
+    model = nn.Sequential(
+        nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Flatten(),
+        nn.Linear(14*14*32, 10)
+    )
+    print(f"Training with {optimizer.__name__}")
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1,2,1)
+    if optimizer == CustomZeroOrderOptimizer:
+        loss_curve, valid_curve = zeroth_order_loop(train_loader, valid_loader, model, loss_fn, optimizer)
+        plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Loss')
+        plt.subplot(1,2,2)
+        plt.plot(valid_curve, label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Validation Accuracy')
+        plt.legend()
+        plt.savefig(f'loss_curve_{optimizer.__name__}_img_noavg.png')
+        continue
+    try:
+        loss_curve, valid_curve = two_loop_optimizer(train_loader, valid_loader, model, loss_fn, optimizer[0], optimizer[1])
+        plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Loss')
+        plt.subplot(1,2,2)
+        plt.plot(valid_curve, label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Validation Accuracy')
+        plt.legend()
+        plt.savefig(f'loss_curve_{optimizer[0].__name__}_{optimizer[1].__name__}_img_noavg.png')
+        continue
+    except:
+        pass
+    loss_curve, valid_curve = loop(train_loader, valid_loader,model, loss_fn, optimizer)
+    plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+    plt.xlabel('Batch')
+    plt.ylabel('Loss')
+    plt.subplot(1,2,2)
+    plt.plot(valid_curve, label=optimizer.__name__)
+    plt.xlabel('Batch')
+    plt.ylabel('Validation Accuracy')
+    plt.legend()
+    plt.savefig(f'loss_curve_{optimizer.__name__}_img_noavg.png')
+    continue
+for i,optimizer in enumerate(optimizers):
+    model = nn.Sequential(
+        nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Flatten(),
+        nn.Linear(14*14*32, 10)
+    )
+    print(f"Training with {optimizer.__name__}")
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1,2,1)
+    if optimizer == CustomZeroOrderOptimizer:
+        loss_curve, valid_curve = zeroth_order_loop(train_loader, valid_loader, model, loss_fn, optimizer,averaging=True)
+        plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Loss')
+        plt.subplot(1,2,2)
+        plt.plot(valid_curve, label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Validation Accuracy')
+        plt.legend()
+        plt.savefig(f'loss_curve_{optimizer.__name__}_img_avg.png')
+        continue
+    try:
+        loss_curve, valid_curve = two_loop_optimizer(train_loader, valid_loader, model, loss_fn, optimizer[0], optimizer[1])
+        plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Loss')
+        plt.subplot(1,2,2)
+        plt.plot(valid_curve, label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Validation Accuracy')
+        plt.legend()
+        plt.savefig(f'loss_curve_{optimizer[0].__name__}_{optimizer[1].__name__}_img_noavg.png')
+        continue
+    except:
+        pass
+    loss_curve, valid_curve = loop(train_loader, valid_loader, model, loss_fn, optimizer,averaging=True)
+
+    plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+    plt.xlabel('Batch')
+    plt.ylabel('Loss')
+    plt.subplot(1,2,2)
+    plt.plot(valid_curve, label=optimizer.__name__)
+    plt.xlabel('Batch')
+    plt.ylabel('Validation Accuracy')
+    plt.legend()
+    plt.savefig(f'loss_curve_{optimizer.__name__}_img_avg.png')
+    continue
+class RNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
+        super(RNN, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.rnn = nn.RNN(embedding_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, 2)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x, _ = self.rnn(x)
+        x = self.fc(x[:, -1, :])
+        return x
+loss_fn = nn.CrossEntropyLoss()
+from imdb import train_dataloader, val_dataloader
+train_loader = train_dataloader
+valid_loader = val_dataloader
+for i,optimizer in enumerate(optimizers):
+    rnn = RNN(vocab_size, 128, 128)
+    print(f"Training with {optimizer.__name__}")
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1,2,1)
+    if optimizer == CustomZeroOrderOptimizer:
+        loss_curve, valid_curve = zeroth_order_loop(train_loader, valid_loader, rnn, loss_fn, optimizer)
+        plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Loss')
+        plt.subplot(1,2,2)
+        plt.plot(valid_curve, label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Validation Accuracy')
+        plt.legend()
+        plt.savefig(f'loss_curve_{optimizer.__name__}_txt_noavg.png')
+        continue
+    try:
+        loss_curve, valid_curve = two_loop_optimizer(train_loader, valid_loader, rnn, loss_fn, optimizer[0], optimizer[1])
+
+        plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Loss')
+        plt.subplot(1,2,2)
+        plt.plot(valid_curve, label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Validation Accuracy')
+        plt.legend()
+        plt.savefig(f'loss_curve_{optimizer[0].__name__}_{optimizer[1].__name__}_txt_noavg.png')
+        continue
+    except:
+        pass
+    loss_curve, valid_curve = loop(train_loader, valid_loader, rnn, loss_fn, optimizer,averaging=True)
+    plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+    plt.xlabel('Batch')
+    plt.ylabel('Loss')
+    plt.subplot(1,2,2)
+    plt.plot(valid_curve, label=optimizer.__name__)
+    plt.xlabel('Batch')
+    plt.ylabel('Validation Accuracy')
+    plt.legend()
+    plt.savefig(f'loss_curve_{optimizer.__name__}_txt_noavg.png')
+    continue
 for i,optimizer in enumerate(optimizers):
     print(f"Training with {optimizer.__name__}")
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1,2,1)
     if optimizer == CustomZeroOrderOptimizer:
-        loss_curve = zeroth_order_loop(train_loader, model, loss_fn, optimizer)
-    else:
-    #    loss_curve = loop(train_loader, model, loss_fn, optimizer,averaging=True)
+        loss_curve, valid_curve = zeroth_order_loop(train_loader, valid_loader, model, loss_fn, optimizer,averaging=True)
+        plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Loss')
+        plt.subplot(1,2,2)
+        plt.plot(valid_curve, label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Validation Accuracy')
+        plt.legend()
+        plt.savefig(f'loss_curve_{optimizer.__name__}_img_avg.png')
         continue
-  #  plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
-plt.title('Loss Curve')
-plt.xlabel('Batch')
-plt.ylabel('Loss')
-plt.savefig('loss_curve.png')
-
-rnn = nn.Sequential(
-    nn.Embedding(len(vocab), 50, padding_idx=vocab['<pad>']),
-    nn.RNN(50, 128, batch_first=True),
-    nn.Linear(128, 2)
-)
-loss_fn = nn.CrossEntropyLoss()
-loss_curve = loop(train_dataloader, rnn, loss_fn, CustomOptimizer, lr=0.01, epochs=1)
-plt.plot(moving_average(loss_curve,100), label='RNN')
-plt.savefig('loss_curve_rnn.png')
+    try:
+        loss_curve, valid_curve = two_loop_optimizer(train_loader, valid_loader, model, loss_fn, optimizer[0], optimizer[1])
+        plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Loss')
+        plt.subplot(1,2,2)
+        plt.plot(valid_curve, label=optimizer.__name__)
+        plt.xlabel('Batch')
+        plt.ylabel('Validation Accuracy')
+        plt.legend()
+        plt.savefig(f'loss_curve_{optimizer[0].__name__}_{optimizer[1].__name__}_img_noavg.png')
+        continue
+    except:
+        pass
+    loss_curve, valid_curve = loop(train_loader, valid_loader, rnn, loss_fn, optimizer,averaging=True)
+    plt.plot(moving_average(loss_curve,100), label=optimizer.__name__)
+    plt.xlabel('Batch')
+    plt.ylabel('Loss')
+    plt.subplot(1,2,2)
+    plt.plot(valid_curve, label=optimizer.__name__)
+    plt.xlabel('Batch')
+    plt.ylabel('Validation Accuracy')
+    plt.legend()
+    plt.savefig(f'loss_curve_{optimizer.__name__}_img_avg.png')
+    continue
